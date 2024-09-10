ba4aec5c79910d7c075b9de040e4f90b4d8334e9
Vidya <vidyavati.gaikwad@dci-student.org>
2024-09-10 22:11:11 +0200

    Python Assignment_1, 05.09.24

---
+++ Task_1.py
@@ -0,0 +1,22 @@
+# Python class assignment Thrusday, 05.09.2024
+# Task 1 - printing single object 
+# Result should look like
+'''
+123
+43.23
+(4-1j)
+How are you?
+True
+'''
+
+number_int = 123
+number_float = 43.23
+number_complex = (4 - 1j)
+sentence = "How are you?"
+
+print(number_int)
+print(number_float)
+print(number_complex)
+print(sentence)
+print(isinstance(number_int,int))
+
